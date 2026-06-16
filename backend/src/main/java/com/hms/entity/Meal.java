19e6461decdb45d4b7e1f176506235458f44c37f
Rinto Cherian <chrint223@gmail.com>
2026-06-16 11:07:16 +0530

    getters and setters addition

--- backend/src/main/java/com/hms/entity/Meal.java
+++ backend/src/main/java/com/hms/entity/Meal.java
@@ -46,7 +46,6 @@ public class Meal {
         this.mealDate = mealDate;
     }
     
-    // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     
